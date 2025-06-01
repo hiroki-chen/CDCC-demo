984cb0ace964b0b99b0b986594f87865cad892b1
Hiroki Chen <haobchen@iu.edu>
2025-06-01 10:12:01 -0400

    Update

--- policy-styx-lib/src/tdx_attestation.rs
+++ policy-styx-lib/src/tdx_attestation.rs
@@ -7,17 +7,24 @@ use tdx::*;
 #[cfg(feature = "platform-tdx")]
 #[inline]
 /// Fetch the TDX attestation report from `/dev/tdx-guest`.
-/// 
+///
 /// # Note
-/// 
+///
 /// This is guest-side function which means this function will directly retrieve the
 /// report from the special device created by the tdx module for the patched guest
 /// kernel. Calling this function on the host will result in an error.
 pub fn get_tdx_attestation_report() -> Result<QuoteV4> {
+    let uid = unsafe { libc::getuid() };
+    // Make sure we are root.
+    if uid != 0 {
+        return Err(anyhow!("Must run in root! Your user id is {uid}"));
+    }
+
     // Talk with the TDX host to fetch its report.
     let tdx = Tdx::new();
 
-    tdx.get_attestation_report().map_err(|e| anyhow!(e))
+    tdx.get_attestation_report()
+        .map_err(|e| anyhow!("Cannot obtain the report"))
 }
 
 #[cfg(test)]
@@ -27,6 +34,12 @@ mod test {
     #[cfg(feature = "platform-tdx")]
     #[test]
     fn test_get_tdx_attestation_report() {
-        assert!(get_tdx_attestation_report().is_ok())
+        let report = get_tdx_attestation_report();
+
+        assert!(
+            report.is_ok(),
+            "Failed to fetch the report: {}",
+            report.err().unwrap()
+        );
     }
 }
